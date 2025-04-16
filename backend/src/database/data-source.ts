7e064c69246fd62d2776602c23baec63b2fdf435
kacper0276 <kacper0276@op.pl>
2025-04-16 22:28:33 +0200

    [BE] Create basic configs and decorators

---
+++ backend/src/database/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST || 'localhost',
+  port: +process.env.DB_PORT || 3307,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'test',
+  synchronize: false,
+  logging: true,
+  entities: [],
+  migrations: ['migrations/**/*.ts'],
+  migrationsTableName: 'migrations_typeorm',
+});
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Pracownia Dyplomowa I - Swagger')
+    .setDescription('Swagger przygotowany dla testowania API')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory(), {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  app.enableCors({
+    origin: ['http://127.0.0.1:4200', 'http://localhost:4200'],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
+  app.useWebSocketAdapter(new IoAdapter(app));
+
+  app.use(cookieParser());
+
+  app.use(bodyParser.json({ limit: '10mb' }));
+
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
