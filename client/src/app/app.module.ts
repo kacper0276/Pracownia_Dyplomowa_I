4cb3f02f9386d80eab96cf714ecc3d292f36b392
kacper0276 <kacper0276@op.pl>
2025-04-24 09:18:19 +0200

    [FE] Add notification package

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
--- client/src/app/modules/layout/components/layout.component.ts
+++ client/src/app/modules/layout/components/layout.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {
-
+export class LayoutComponent implements OnInit {
+  constructor(private toastr: ToastrService) {}
+  ngOnInit(): void {
+    this.toastr.success('Hello world!', 'Toastr fun!');
+  }
 }
