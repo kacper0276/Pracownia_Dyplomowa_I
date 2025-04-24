7cdcfb89bc96ba00118c0428743943f26ef4f709
kacper0276 <kacper0276@op.pl>
2025-04-24 09:01:07 +0200

    [FE] Create models for login

---
+++ client/src/app/shared/models/index.ts
@@ -0,0 +1,3 @@
+export * from './api-response.model';
+export * from './login-response.model';
+export * from './user.model';
---
+++ client/src/app/shared/models/user.model.ts
@@ -0,0 +1,8 @@
+export interface User {
+  id: number;
+  email: string;
+  login: string;
+  password: string;
+  isActive: boolean;
+  isOnline: boolean;
+}
