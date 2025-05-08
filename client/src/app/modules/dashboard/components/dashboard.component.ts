bde0fe00d046354cdbbb55726d30ace05c652cdf
kacper0276 <kacper0276@op.pl>
2025-05-08 08:48:02 +0200

    [FE] Implement user search functionality in dashboard component

--- client/src/app/modules/dashboard/components/dashboard.component.ts
+++ client/src/app/modules/dashboard/components/dashboard.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import { UserService } from '../../../shared/services/user.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   showModal = false;
-  currentUserId = 1;
+  searchControl = new FormControl('');
+  users: any[] = [];
+
+  constructor(private readonly userService: UserService) {}
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap((query) => this.userService.searchUsers(query ?? ''))
+      )
+      .subscribe((users) => {
+        this.users = users.data ?? [];
+      });
+  }
 
   toggleShowModal(): void {
     this.showModal = !this.showModal;
