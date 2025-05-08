c1a0e9bbbb68b4b8ccf0293dd322b644fbd19483
kacper0276 <kacper0276@op.pl>
2025-05-08 09:13:07 +0200

    [FE] Implement user profile component with friends and posts display

--- client/src/app/modules/profile/components/user-profile/user-profile.component.ts
+++ client/src/app/modules/profile/components/user-profile/user-profile.component.ts
@@ -3,8 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss'
+  styleUrl: './user-profile.component.scss',
 })
 export class UserProfileComponent {
-
+  userProfile = {
+    email: 'Test@test.pl',
+    name: 'TEST',
+    surname: 'Testowy',
+    bio: 'AAAADASDASDASDKASO D IAS DOASIDJ ASOID',
+    isActive: true,
+    friends: [
+      {
+        id: 1,
+        name: 'Jan',
+        surname: 'Kowalski',
+        email: '',
+      },
+    ],
+    posts: [
+      {
+        id: 1,
+        title: 'Post 1',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        date: new Date('2023-10-01'),
+      },
+      {
+        id: 2,
+        title: 'Post 2',
+        content:
+          'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        date: new Date('2023-10-02'),
+      },
+      {
+        id: 1,
+        title: 'Post 1',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        date: new Date('2023-10-01'),
+      },
+      {
+        id: 2,
+        title: 'Post 2',
+        content:
+          'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        date: new Date('2023-10-02'),
+      },
+    ],
+  };
 }
