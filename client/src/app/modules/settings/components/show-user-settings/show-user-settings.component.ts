0b3413b54cc0eeef1f2a43f891e79427204e5070
kacper0276 <kacper0276@op.pl>
2025-05-15 21:25:32 +0200

    [FE] Create settings form

--- client/src/app/modules/settings/components/show-user-settings/show-user-settings.component.ts
+++ client/src/app/modules/settings/components/show-user-settings/show-user-settings.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-show-user-settings',
   templateUrl: './show-user-settings.component.html',
-  styleUrl: './show-user-settings.component.scss'
+  styleUrl: './show-user-settings.component.scss',
 })
-export class ShowUserSettingsComponent {
+export class ShowUserSettingsComponent implements OnInit {
+  settingsForm!: FormGroup;
+  profileImageBase64: string | null = null;
+  backgroundImageBase64: string | null = null;
+  showPassword = false;
 
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    const user = this.authService.getUser();
+    this.settingsForm = this.fb.group({
+      firstName: [user?.firstName || ''],
+      lastName: [user?.lastName || ''],
+      login: [user?.login || '', Validators.required],
+      password: [''],
+      profileImage: [user?.profileImage || null],
+      backgroundImage: [user?.backgroundImage || null],
+    });
+
+    this.profileImageBase64 = user?.profileImage || null;
+    this.backgroundImageBase64 = user?.backgroundImage || null;
+  }
+
+  onFileChange(event: Event, type: 'profileImage' | 'backgroundImage'): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        const base64 = reader.result as string;
+        if (type === 'profileImage') {
+          this.profileImageBase64 = base64;
+          this.settingsForm.patchValue({ profileImage: base64 });
+        } else {
+          this.backgroundImageBase64 = base64;
+          this.settingsForm.patchValue({ backgroundImage: base64 });
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  onSubmit(): void {
+    if (this.settingsForm.valid) {
+      const formValue = this.settingsForm.value;
+    }
+  }
 }
--- client/src/app/modules/settings/settings.module.ts
+++ client/src/app/modules/settings/settings.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { ShowUserSettingsComponent } from './components/show-user-settings/show-user-settings.component';
-
+import { SharedModule } from '../../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    ShowUserSettingsComponent
-  ],
-  imports: [
-    CommonModule,
-    SettingsRoutingModule
-  ]
+  declarations: [ShowUserSettingsComponent],
+  imports: [SharedModule, SettingsRoutingModule, ReactiveFormsModule],
 })
-export class SettingsModule { }
+export class SettingsModule {}
