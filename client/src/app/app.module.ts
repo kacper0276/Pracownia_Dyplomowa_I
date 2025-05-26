aff1b6b18f1a60976ba573f595dc9cbb3e109ec2
kacper0276 <kacper0276@op.pl>
2025-05-26 02:04:03 +0200

    [FE] Add translations

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  HttpClientModule,
+} from '@angular/common/http';
 import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +28,15 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     SharedModule,
     ToastrModule.forRoot(),
     BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'pl',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [
     {
--- client/src/app/modules/layout/components/side-nav/side-nav.component.ts
+++ client/src/app/modules/layout/components/side-nav/side-nav.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AuthService } from '../../../../shared/services/auth.service';
 import { User } from '../../../../shared/models';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'side-nav',
@@ -17,7 +18,8 @@ export class SideNavComponent {
 
   constructor(
     private readonly authService: AuthService,
-    private readonly router: Router
+    private readonly router: Router,
+    private readonly translate: TranslateService
   ) {
     this.user = this.authService.getUser();
   }
@@ -27,7 +29,9 @@ export class SideNavComponent {
     this.toggleNav.emit(this.isCollapsed);
   }
 
-  changeLanguage(lang: string): void {}
+  changeLanguage(lang: string): void {
+    this.translate.use(lang);
+  }
 
   logout(): void {
     this.authService.logout();
